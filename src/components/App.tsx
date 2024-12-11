8b87918cf798ecdad4893a491b77a1ba0c93ff42
mbreka <mislawbreka@gmail.com>
2024-12-11 01:58:51 +0100

    add react ts config

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+import "./App.css";
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <img src={'/logo.svg'} alt="" srcSet="" />
+      <a href="http://" target="_blank" rel="noopener noreferrer">
+        learn react
+      </a>
+    </div>
+  );
+};
+
+export default App;
