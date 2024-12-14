a130e09707629600c42104f52d43291da1df87b5
mbreka <mislawbreka@gmail.com>
2024-12-14 22:02:39 +0100

    Small updates

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,19 +1,68 @@
-import React, { FC, useContext } from "react";
-import { Table } from "@mantine/core";
+import React, { FC, useContext, useEffect, useState } from "react";
+import { ActionIcon, Table } from "@mantine/core";
 import { CartMeta, ProductI } from "@/types/interfaces";
 import { AppContext } from "@/providers/AppProvider";
+import { IconChevronDown, IconChevronUp } from "@tabler/icons-react";
+import { Link } from "react-router";
+
+import { ProductModal } from "./ProductModal";
+
+const CartRow: FC<{ product: CartMeta<ProductI> }> = ({ product }) => {
+  const { selected, setSelected, totalProducts, cart, getProductCartMeta, removeProductFromCart, addProductToCart } =
+    useContext(AppContext)!;
+
+  return (
+    <>
+      <Table.Tr key={product.item.id}>
+        <Table.Td
+          onClick={() => {
+            setSelected(product.item);
+          }}
+        >
+          {product.item.title}
+        </Table.Td>
+        <Table.Td>{product.item.price}$</Table.Td>
+        <Table.Td>
+          <ActionIcon.Group>
+            <ActionIcon
+              variant="default"
+              size="sm"
+              radius="md"
+              onClick={() => {
+                if (!product) {
+                  return;
+                }
+                removeProductFromCart(product);
+              }}
+            >
+              <IconChevronDown color="var(--mantine-color-red-text)" />
+            </ActionIcon>
+            <ActionIcon.GroupSection variant="default" size="sm" bg="var(--mantine-color-body)" miw={60}>
+              {/* {value} */}
+              {product?.quantity || 0}
+            </ActionIcon.GroupSection>
+            <ActionIcon
+              variant="default"
+              size="sm"
+              radius="md"
+              onClick={() => {
+                if (!product) {
+                  return;
+                }
+                addProductToCart(product);
+              }}
+            >
+              <IconChevronUp color="var(--mantine-color-teal-text)" />
+            </ActionIcon>
+          </ActionIcon.Group>
+        </Table.Td>
+      </Table.Tr>
+    </>
+  );
+};
 
 const CartTable: FC<{ products: CartMeta<ProductI>[] }> = ({ products }) => {
-  const rows =
-    products &&
-    products.map((element) => (
-      <Table.Tr key={element.item.id}>
-        <Table.Td>{element.item.title}</Table.Td>
-        <Table.Td>{element.item.price}$</Table.Td>
-        <Table.Td>{element.quantity}</Table.Td>
-      </Table.Tr>
-    ));
-
+  const rows = products && products.map((p, i) => <CartRow product={p} key={i} />);
   return (
     <Table stickyHeader stickyHeaderOffset={60}>
       <Table.Thead>
@@ -33,6 +82,7 @@ const Cart: FC = () => {
   const { login, users, cart } = useContext(AppContext)!;
   return (
     <>
+      <ProductModal />
       <CartTable products={cart} />
     </>
   );
--- src/components/ProductModal.tsx
+++ src/components/ProductModal.tsx
@@ -1,10 +1,11 @@
-import React, { FC, useContext } from "react";
+import React, { FC, useContext, useEffect, useState } from "react";
 import { useDisclosure } from "@mantine/hooks";
-import { Modal, Button, Text, Image } from "@mantine/core";
+import { Modal, Button, Text, Image, ActionIcon, Badge, Box, Card, Group, rem } from "@mantine/core";
 import { AppContext } from "@/providers/AppProvider";
 import { getProducts } from "@/api";
-import { ProductI, ReviewI } from "@/types/interfaces";
+import { CartMeta, ProductI, ReviewI } from "@/types/interfaces";
 import { Carousel } from "@mantine/carousel";
+import { IconEye, IconChevronDown, IconChevronUp } from "@tabler/icons-react";
 
 const ReviewCard: FC<{ review: ReviewI }> = ({ review: { rating, comment, date, reviewerName, reviewerEmail } }) => {
   return (
@@ -18,8 +19,8 @@ const ReviewCard: FC<{ review: ReviewI }> = ({ review: { rating, comment, date, 
   );
 };
 
-const ProductDetails: FC<{ product: ProductI }> = ({
-  product: {
+const ProductDetails: FC<{ product: ProductI }> = ({ product }) => {
+  const {
     id,
     title,
     description,
@@ -42,40 +43,134 @@ const ProductDetails: FC<{ product: ProductI }> = ({
     meta: { createdAt, updatedAt, barcode, qrCode },
     images,
     thumbnail,
-  },
-}) => {
+  } = product;
+
+  const { selected, setSelected, totalProducts, cart, getProductCartMeta, removeProductFromCart, addProductToCart } =
+    useContext(AppContext)!;
+
+  const [productMeta, setProductMeta] = useState<CartMeta<ProductI>>();
+  useEffect(() => {
+    // console.log({ productMeta });
+
+    setProductMeta(getProductCartMeta(product));
+  }, [product, cart]);
+
   return (
     <>
-      <Text>{title}</Text>
-      <Text>{description}</Text>
-      <Text>{category}</Text>
-      <Text>{price}$</Text>
-      <Text>{discountPercentage}</Text>
-      <Text>{rating}</Text>
-      <Text>{stock}</Text>
-      <Text>{tags}</Text>
-      <Text>{brand}</Text>
-      <Text>{sku}</Text>
-      <Text>{weight}</Text>
-      <Text>{warrantyInformation}</Text>
-      <Text>{shippingInformation}</Text>
-      <Text>{availabilityStatus}</Text>
-      <Text>{returnPolicy}</Text>
-      <Text>{minimumOrderQuantity}</Text>
+      <Card shadow="sm" padding="lg" radius="md" withBorder style={{ height: "100%", justifyContent: "space-between" }}>
+        <Card.Section>
+          <Carousel withIndicators height={"auto"}>
+            {images.map((src, index) => {
+              return (
+                <Carousel.Slide key={src}>
+                  <Image key={index} src={src} />;
+                </Carousel.Slide>
+              );
+            })}
+          </Carousel>
+        </Card.Section>
 
-      <Carousel withIndicators height={"auto"}>
-        {images.map((src, index) => {
-          return (
-            <Carousel.Slide key={src}>
-              <Image key={index} src={src} />;
-            </Carousel.Slide>
-          );
-        })}
-      </Carousel>
+        <Group
+          onClick={() => {
+            setSelected(product);
+          }}
+          justify="space-between"
+          mt="md"
+          mb="xs"
+        >
+          <Text fw={500}>{title}</Text>
+        </Group>
+        <Group justify="space-between" mt="md" mb="xs">
+          <Text size="xl" fw={500}>{price}$</Text>
+          {discountPercentage > 0 && <Badge color="pink">-{discountPercentage}%</Badge>}
+        </Group>
 
-      {reviews.map((review, index) => {
-        return <ReviewCard key={index} review={review} />;
-      })}
+        <Text
+          onClick={() => {
+            setSelected(product);
+          }}
+          size="sm"
+          c="dimmed"
+        >
+          {description}
+        </Text>
+
+        <Box
+          mt={rem(20)}
+          w={"100%"}
+          style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}
+        >
+          {/* <ActionIcon
+            color="blue"
+            radius="md"
+            onClick={() => {
+              setSelected(product);
+            }}
+          >
+            <IconEye />
+          </ActionIcon> */}
+
+          <ActionIcon.Group>
+            <ActionIcon
+              variant="default"
+              size="md"
+              radius="md"
+              onClick={() => {
+                if (!productMeta) {
+                  return;
+                }
+                removeProductFromCart(productMeta);
+              }}
+            >
+              <IconChevronDown color="var(--mantine-color-red-text)" />
+            </ActionIcon>
+            <ActionIcon.GroupSection variant="default" size="md" bg="var(--mantine-color-body)" miw={60}>
+              {/* {value} */}
+              {productMeta?.quantity || 0}
+            </ActionIcon.GroupSection>
+            <ActionIcon
+              variant="default"
+              size="md"
+              radius="md"
+              onClick={() => {
+                if (!productMeta) {
+                  return;
+                }
+                addProductToCart(productMeta);
+              }}
+            >
+              <IconChevronUp color="var(--mantine-color-teal-text)" />
+            </ActionIcon>
+          </ActionIcon.Group>
+
+          <Group justify="space-between" mt="md" mb="xs">
+            <Box></Box>
+            <Box></Box>
+          </Group>
+          {/* <Box>
+            <Text>{title}</Text>
+            <Text>{description}</Text>
+            <Text>{category}</Text>
+            <Text>{price}$</Text>
+            <Text>{discountPercentage}</Text>
+            <Text>{rating}</Text>
+            <Text>{stock}</Text>
+            <Text>{tags}</Text>
+            <Text>{brand}</Text>
+            <Text>{sku}</Text>
+            <Text>{weight}</Text>
+            <Text>{warrantyInformation}</Text>
+            <Text>{shippingInformation}</Text>
+            <Text>{availabilityStatus}</Text>
+            <Text>{returnPolicy}</Text>
+            <Text>{minimumOrderQuantity}</Text>
+          </Box>
+
+          {reviews.map((review, index) => {
+            return <ReviewCard key={index} review={review} />;
+          })} */}
+        </Box>
+      </Card>
     </>
   );
 };
